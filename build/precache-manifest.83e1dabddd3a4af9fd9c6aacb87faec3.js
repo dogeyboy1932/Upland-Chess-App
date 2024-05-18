4e9fdd251189ed861a3e94273cf9ffad8d26a0f0
dogeyboy1932 <vg23@illinois.edu>
2024-05-18 00:18:26 -0500

    Just Frontend

---
+++ build/precache-manifest.83e1dabddd3a4af9fd9c6aacb87faec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31dc4f8018a09c421c361ba5017edbf1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7470b1315212c470a3d6",
+    "url": "./static/css/main.a8c67c1e.chunk.css"
+  },
+  {
+    "revision": "166f01b834bb5a0d309b",
+    "url": "./static/js/2.f0dda4e6.chunk.js"
+  },
+  {
+    "revision": "023d10f1bef133c9d21a52a0b1d7d497",
+    "url": "./static/js/2.f0dda4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7470b1315212c470a3d6",
+    "url": "./static/js/main.8950a9e4.chunk.js"
+  },
+  {
+    "revision": "62afcf799ffb4f7a2534",
+    "url": "./static/js/runtime-main.ebd6abeb.js"
+  }
+]);
