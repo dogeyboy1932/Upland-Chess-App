8e94bbe770ccca55f5c34ab60d8afef4dab47ee1
dogeyboy1932 <vg23@illinois.edu>
2024-05-17 23:47:59 -0500

    Changed Backend Domain Name

--- Frontend/src/FIXED_FRONTEND_VARIABLES.js
+++ Frontend/src/FIXED_FRONTEND_VARIABLES.js
@@ -1,2 +1,1 @@
-export const BASE_URL = 'https://akhil-python-test-0b32b2533b48.herokuapp.com/'
-// export const BASE_URL = 'http://127.0.0.1:4000'
+export const BASE_URL = 'https://akhil-upland-chess-d17596495d27.herokuapp.com/'
