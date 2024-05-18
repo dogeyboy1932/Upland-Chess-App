8d25d9fe13698f17038ad43346693fb9b5e65368
dogeyboy1932 <vg23@illinois.edu>
2024-05-17 23:19:30 -0500

    Make Better

--- Upland/SpreadsheetEditing/fill_profile.py
+++ Upland/SpreadsheetEditing/fill_profile.py
@@ -11,31 +11,31 @@ from Upland.SpreadsheetEditing.edit_profile import FillingLichessInfo
 def FillProfile(uplandID, lichessID, password):   # Called when you submit a created profile
     print("REACHED HERE")
     
-    worksheet = load_workbook(filepath)['Sheet']
+    # worksheet = load_workbook(filepath)['Sheet']
 
-    # Clearing up errors    
-    lichessRating = GetLichessRating(lichessID, "rapid")
-    if (lichessRating == -1): return 'invalid lichess'
+    # # Clearing up errors    
+    # lichessRating = GetLichessRating(lichessID, "rapid")
+    # if (lichessRating == -1): return 'invalid lichess'
     
-    id_index = QueryUplandIDRow(uplandID)
-    if (id_index == -1): return 'no profile found'
+    # id_index = QueryUplandIDRow(uplandID)
+    # if (id_index == -1): return 'no profile found'
 
 
-    # Getting existing password as a security measure
-    prof_pass = worksheet[id_index][6].value
+    # # Getting existing password as a security measure
+    # prof_pass = worksheet[id_index][6].value
+    # prof_lichess = worksheet[id_index][0].value
 
-
-    # Depending on parameters, the profile will either be edited or errors will be returned
-    if (prof_pass):
-        if (prof_pass == password):
-            if (lichessID != worksheet[id_index][0].value):
-                FillingLichessInfo(id_index, lichessID, lichessRating, -1)
-                return 'replaced'
-            else:
-                return 'same'
-        else:
-            return 'wrong password'         
-    else:
-        FillingLichessInfo(id_index, lichessID, lichessRating, password)
+    # # Depending on parameters, the profile will either be edited or errors will be returned
+    # if (prof_pass):
+    #     if (prof_pass == password):
+    #         if (lichessID != prof_lichess):
+    #             FillingLichessInfo(id_index, lichessID, lichessRating, -1)
+    #             return 'replaced'
+    #         else:
+    #             return 'same'
+    #     else:
+    #         return 'wrong password'         
+    # else:
+    #     FillingLichessInfo(id_index, lichessID, lichessRating, password)
 
     return "success"
