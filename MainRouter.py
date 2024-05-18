da2e3c51a6e2203b63b48511fd15a8f760d603d8
dogeyboy1932 <vg23@illinois.edu>
2024-05-17 22:50:00 -0500

    Make Better

--- MainRouter.py
+++ MainRouter.py
@@ -136,7 +136,7 @@ def ChallengeButton():
     return str(ChallengeButtonClicked(uplandID, rated, wager, speed, variant, name, increment))
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def respond():
     # print("START1\n")
 
