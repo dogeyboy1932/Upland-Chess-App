38c3ac0bbe7783f535b87f9bc2f264669e287c74
dogeyboy1932 <vg23@illinois.edu>
2024-05-22 09:14:41 -0500

    Test3

--- MainRouter.py
+++ MainRouter.py
@@ -1,15 +1,18 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
+import pandas as pd
 import json
+import os
 
 from FIXED_VARIABLES import NumpyArrayEncoder
 
 from Upland.Auth_code import Verify
 from Upland.SpreadsheetEditing.create_profile import CreateProfile
-from Upland.SpreadsheetEditing.edit_profile import DeleteProfile
+from Upland.SpreadsheetEditing.edit_profile import AppendProfileHeader, DeleteProfile
 from Upland.SpreadsheetEditing.fill_profile import FillProfile
 from Upland.SpreadsheetEditing.query_spreadsheet import QueryForPassword, QueryForLichessID, GetCredentialsByID
 from Upland.Escrow.get_escrow_container import GetEscrowContainer
 
+from Chess.NewChallenge.append_challenge import AppendChallengeHeader
 from Chess.ChessDatabase.iterate_database import Iterate, UpdateBalances
 from Chess.ChessDatabase.handle_finished_games import HandleFinishedGames
 from Chess.Buttons.delete_button import ChallengeDeleted
@@ -25,7 +28,6 @@ app = Flask(__name__)
 CORS(app)
 app.logger.disabled = True
 
-
 @app.route('/database', methods=['POST'])
 def ChallengeDatabase():
     
@@ -37,41 +39,36 @@ def ChallengeDatabase():
 
     return encodedNumpyData
 
-    # return []
-
 
 @app.route('/auth', methods=['POST'])
 def Auth():
-    # print("AUTH1")
     return Verify()
 
 
 @app.route('/password', methods=['POST'])
 def Password():
-    uplandID = request.get_json().get('uplandID')
+    uplandID = request.json.get('uplandID')
 
     return str(QueryForPassword(uplandID))
-
-    # return "-1"
 
 
 @app.route('/getLichessID', methods=['POST'])
 def GetLichessID():
-    uplandID = request.get_json().get('uplandID')
+    uplandID = request.json.get('uplandID')
 
     return str(QueryForLichessID(uplandID))
 
 
 @app.route('/getLichessInfo', methods=['POST'])
 def GetLichessInfo():
-    lichessID = request.get_json().get('lichessId')
+    lichessID = request.json.get('lichessId')
 
     return jsonify(GetVariant(lichessID, "rapid"))
 
 
 @app.route('/getEscrow', methods=['POST'])
 def GetEscrow():
-    escrowId = request.get_json().get('escrowId')
+    escrowId = request.json.get('escrowId')
 
     # print(jsonify(GetEscrowContainer(escrowId)))
 
@@ -82,8 +79,8 @@ def GetEscrow():
 def Accepted():
     Iterate()
 
-    accepter = request.get_json().get('currentUserUplandID')
-    link = request.get_json().get('link')
+    accepter = request.json.get('currentUserUplandID')
+    link = request.json.get('link')
 
     res = ChallengeAccepted(link, accepter)
         
@@ -92,7 +89,7 @@ def Accepted():
 
 @app.route('/cancel', methods=['POST'])
 def Cancel():
-    link = request.get_json().get('link')
+    link = request.json.get('link')
     
     ChallengeCanceled(link)
     return "Success"
@@ -100,33 +97,32 @@ def Cancel():
 
 @app.route('/delete', methods=['POST'])
 def Deleted():
-    link = request.get_json().get('link')
+    link = request.json.get('link')
 
     return ChallengeDeleted(link)
     
 
-
 @app.route('/credentials', methods=['POST'])
 def Credentials():
-    uplandID = request.get_json().get('uplandID')
-    lichessID = request.get_json().get('lichessID')
-    password = request.get_json().get('password')
+    uplandID = request.json.get('uplandID')
+    lichessID = request.json.get('lichessID')
+    password = request.json.get('password')
 
     return FillProfile(uplandID, lichessID, password)   
 
 
 @app.route('/deleteProfile', methods=['POST'])
 def DeleteProf():
-    uplandID = request.get_json().get('uplandIDRemove')
-    password = request.get_json().get('passwordRemove')
+    uplandID = request.json.get('uplandIDRemove')
+    password = request.json.get('passwordRemove')
 
-    return str(DeleteProfile(uplandID, password))
+    return DeleteProfile(uplandID, password)  
 
 
 @app.route('/submit-details', methods=['POST'])
 def ChallengeButton():
     
-    data = request.get_json()
+    data = request.json
 
     uplandID = data.get('upland')
     rated = data.get('rated')
@@ -141,16 +137,19 @@ def ChallengeButton():
 
 @app.route('/connect', methods=['POST'])
 def respond():
+
     try:
         data = request.json
-        var = data['type']
     except:
         print("NOT VALID REQUEST")
         return str(-1)
     
-    # print(data)
+    var = data.get('type', None)
+    if var is None:
+        return "No data type"
 
-    if data['type'] == 'AuthenticationSuccess':
+
+    if var == 'AuthenticationSuccess':
         access_token = data['data']['accessToken']
         CreateProfile(access_token)
 
@@ -158,14 +157,12 @@ def respond():
         # df = pd.read_excel(filepath)
         # print(df)
     
-    elif data['type'] == 'UserDisconnectedApplication':
+    elif var == 'UserDisconnectedApplication':
         credentials = GetCredentialsByID(data['data']['userId'])
-
-        print(credentials)
 
         if credentials == -1: 
             print("UNABLE TO DELETE PROFILE")
-            return str(-1)
+            return
 
         uplandId = credentials[0]
         password = credentials[1]
@@ -182,7 +179,6 @@ def test():
         "about" :"Hello! I'm a python stack developer"
     }
 
-
     if (request.json):
         return request.json()
 
@@ -190,6 +186,5 @@ def test():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=4000, debug=True)
     app.run()
 
