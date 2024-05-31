24b4d50ae720e09d3acf7b5378f8b25ccca238fb
dogeyboy1932 <vg23@illinois.edu>
2024-05-31 13:31:52 -0500

    Testing Change

--- Upland/get_user_balance.py
+++ Upland/get_user_balance.py
@@ -11,13 +11,20 @@ def GetUserBalance(upland_access_token):
         'Authorization': f'Bearer {upland_access_token}',
     }
 
+    print(upland_access_token)
+
     try:
         conn.request("GET", url, payload, headers)
         
-        res = conn.getresponse()    
+        res = conn.getresponse()
+
+        print(res)
+
         data = json.loads(res.read().decode("utf-8"))
+
+        print(data)
         
-        return data['availableUpx']
+        return data['availableUpx'] 
     except:
         print("getBalanced call failed")
         return -1
